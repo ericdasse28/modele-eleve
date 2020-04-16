d45aa24a4ead02403a19ea33000d53eb8dbac8f7
ericdasse28 <christ.gououi@gmail.com>
2020-04-16 21:57:24 +0200

    Add : classe Eleve

---
+++ src/Eleve.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Eleve {
+	private String nom;
+	private ArrayList<Integer> listeNotes;
+	private double moyenne = 0;
+	
+	
+	public Eleve(String nom) {
+		this.setNom(nom);
+		
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public ArrayList<Integer> getListeNotes() {
+		return listeNotes;
+	}
+
+
+	public void setListeNotes(ArrayList<Integer> listeNotes) {
+		this.listeNotes = listeNotes;
+	}
+
+
+	public double getMoyenne() {
+		return moyenne;
+	}
+
+	
+	public void ajouterNote(int note) {		
+		if (note < 0)
+			note = 0;
+		else if (note > 20)
+			note = 20;
+		
+		listeNotes.add(note);
+		
+		moyenne += note / listeNotes.size();
+		
+	}
+	
+	public String toString() {
+		return nom + "(" + moyenne + ")";
+	}
+
+}
