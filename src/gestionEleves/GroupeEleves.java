dcf6188205fc850eb7e7af5fbce9e9dc483162b4
ericdasse28 <christ.gououi@gmail.com>
2020-04-17 19:46:51 +0200

    Renommage de la classe GestionEleves en GroupeEleves

---
+++ src/gestionEleves/GroupeEleves.java
@@ -0,0 +1,47 @@
+package gestionEleves;
+
+import java.util.ArrayList;
+
+public class GroupeEleves {
+	
+	private ArrayList<Eleve> listeEleves = new ArrayList<>();
+	
+	
+	public int nombre() {
+		return listeEleves.size();
+		
+	}
+	
+	
+	public ArrayList<Eleve> getListe() {
+		return listeEleves;
+		
+	}
+	
+	
+	public void ajouterEleve(Eleve eleve) {
+		listeEleves.add(eleve);
+		
+	}
+	
+	
+	public Eleve chercher(String nom) {
+		
+		for (Eleve eleve : listeEleves) {
+			if (eleve.getNom().equals(nom))
+				return eleve;
+		}
+		
+		return null;
+		
+	}
+	
+	
+	public void lister() {
+		System.out.println("Liste des élèves :");
+		for (Eleve eleve : listeEleves)
+			System.out.println(eleve);
+		
+	}
+
+}
